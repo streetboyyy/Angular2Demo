f8ef12550514c6be176e8284d9a1a52ed295baa3
Miroslav Dimitrov <miroslav.dimitrov@scalefocus.com>
2016-12-05 17:12:09 +0200

    delete templete

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -4,18 +4,7 @@ import { Hero } from '../hero';
 @Component({
   selector: 'app-hero-detail',
   templateUrl: './hero-detail.component.html',
-  styleUrls: ['./hero-detail.component.css'],
-  template: `
-  <div *ngIf="hero">
-    <h2>{{hero.name}} details!</h2>
-    <div><label>id: </label>{{hero.id}}</div>
-    <div>
-      <label>name: </label>
-      <input [(ngModel)]="hero.name" placeholder="name"/>
-    </div>
-  </div>
-`
-
+  styleUrls: ['./hero-detail.component.css']
 })
 export class HeroDetailComponent {
   @Input()
